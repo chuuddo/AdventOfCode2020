f5e18639b89f980b13f2d77535c713f8482fe6c5
Ilya Chudin <chudin.ilya@gmail.com>
2020-12-07 11:52:34 +0400

    add day07

---
+++ day07/solution.js
@@ -0,0 +1,41 @@
+const parseContains = (items) => {
+  return items.split(", ").reduce((res, item) => {
+    const [, count, color] = item.match(/^(\d+)\s(\w+\s\w+)/);
+    res[color] = Number(count);
+    return res;
+  }, {});
+};
+
+const parseGraph = (data) => {
+  return data.reduce((res, line) => {
+    const [color, contains] = line.split(" bags contain ");
+    res[color] = contains === "no other bags." ? {} : parseContains(contains);
+    return res;
+  }, {});
+};
+
+module.exports = {
+  part1: (data) => {
+    const graph = parseGraph(data);
+    return Object.keys(graph).reduce((count, color) => {
+      const stack = Object.keys(graph[color]);
+      while (stack.length > 0) {
+        const current = stack.pop();
+        if (current === "shiny gold") {
+          return count + 1;
+        }
+        stack.push(...Object.keys(graph[current]));
+      }
+      return count;
+    }, 0);
+  },
+  part2: (data) => {
+    const graph = parseGraph(data);
+    const count = (contains) => {
+      return Object.keys(contains).reduce((res, color) => {
+        return res + contains[color] * (1 + count(graph[color]));
+      }, 0);
+    };
+    return count(graph["shiny gold"]);
+  },
+};
---
+++ day07/test.js
@@ -0,0 +1,29 @@
+const { readFileSync } = require("fs");
+const { resolve } = require("path");
+const { part1, part2 } = require("./solution");
+
+const data = readFileSync(resolve(__dirname, "data.txt"), "utf8").trim().split("\n");
+const example = `light red bags contain 1 bright white bag, 2 muted yellow bags.
+dark orange bags contain 3 bright white bags, 4 muted yellow bags.
+bright white bags contain 1 shiny gold bag.
+muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.
+shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.
+dark olive bags contain 3 faded blue bags, 4 dotted black bags.
+vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.
+faded blue bags contain no other bags.
+dotted black bags contain no other bags.`.split("\n");
+
+const example2 = `shiny gold bags contain 2 dark red bags.
+dark red bags contain 2 dark orange bags.
+dark orange bags contain 2 dark yellow bags.
+dark yellow bags contain 2 dark green bags.
+dark green bags contain 2 dark blue bags.
+dark blue bags contain 2 dark violet bags.
+dark violet bags contain no other bags.`.split("\n");
+
+describe("Day 07", () => {
+  test("part 1 example", () => expect(part1(example)).toBe(4));
+  test("part 2 example", () => expect(part2(example2)).toBe(126));
+  test("part 1 data", () => expect(part1(data)).toBe(235));
+  test("part 2 data", () => expect(part2(data)).toBe(158493));
+});
