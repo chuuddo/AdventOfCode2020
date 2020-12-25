40678815b9ccce4febd40ca298c73d2494723d65
Ilya Chudin <chudin.ilya@gmail.com>
2020-12-25 10:13:55 +0400

    day25 simplify

--- day25/solution.js
+++ day25/solution.js
@@ -1,25 +1,13 @@
-const getLoopSize = (subject, target) => {
-  let current = 1;
-  let loop = 0;
-  while (current !== target) {
-    current = (current * subject) % 20201227;
-    loop++;
-  }
-  return loop;
-};
-
-const transform = (subject, loopSize) => {
-  let current = 1;
-  for (let i = 0; i < loopSize; i++) {
-    current = (current * subject) % 20201227;
-  }
-  return current;
-};
-
 module.exports = {
   part1: (data) => {
-    const [door, card] = data.split("\n").map(Number);
-    return transform(card, getLoopSize(7, door));
+    const [card, door] = data.split("\n").map(Number);
+    let key = 1;
+    let target = 1;
+    while (target !== door) {
+      target = (target * 7) % 20201227;
+      key = (key * card) % 20201227;
+    }
+    return key;
   },
   part2: () => 0,
 };
